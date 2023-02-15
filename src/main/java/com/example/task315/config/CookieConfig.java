1a62098ba3da651fdf9ffeef3e2e6f81648cd634
Viktoriya Ilinskaya <viktorinka2121@gmail.com>
2023-02-15 10:59:07 +0300

    Initial commit

---
+++ src/main/java/com/example/task315/config/CookieConfig.java
@@ -0,0 +1,15 @@
+package com.example.task315.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ComponentScan("com.example.task315")
+public class CookieConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
